8daf563020c44f26b029775ad871dd522c90ff34
mikaelnet <github@mail.mikael.com>
2022-05-11 15:30:38 +0200

    Added Desktop UI boilerplate

---
+++ BrfAlbert.Aptus.UI/AptusBillingUI.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace BrfAlbert.Aptus.UI
+{
+    partial class AptusBillingUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AptusBillingUI));
+            this.exitButton = new System.Windows.Forms.Button();
+            this.periodLabel = new System.Windows.Forms.Label();
+            this.periodComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.Location = new System.Drawing.Point(713, 415);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // periodLabel
+            // 
+            this.periodLabel.AutoSize = true;
+            this.periodLabel.Location = new System.Drawing.Point(13, 13);
+            this.periodLabel.Name = "periodLabel";
+            this.periodLabel.Size = new System.Drawing.Size(37, 13);
+            this.periodLabel.TabIndex = 1;
+            this.periodLabel.Text = "Period";
+            // 
+            // periodComboBox
+            // 
+            this.periodComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.periodComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.periodComboBox.FormattingEnabled = true;
+            this.periodComboBox.Location = new System.Drawing.Point(16, 30);
+            this.periodComboBox.Name = "periodComboBox";
+            this.periodComboBox.Size = new System.Drawing.Size(772, 21);
+            this.periodComboBox.TabIndex = 2;
+            // 
+            // AptusBillingUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.periodComboBox);
+            this.Controls.Add(this.periodLabel);
+            this.Controls.Add(this.exitButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AptusBillingUI";
+            this.Text = "Debiteringar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label periodLabel;
+        private System.Windows.Forms.ComboBox periodComboBox;
+    }
+}
+
---
+++ BrfAlbert.Aptus.UI/AptusBillingUI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrfAlbert.Aptus.UI
+{
+    public partial class AptusBillingUI : Form
+    {
+        public AptusBillingUI()
+        {
+            InitializeComponent();
+            InitializePeriods();
+        }
+
+        private void InitializePeriods()
+        {
+            periodComboBox.Items.Clear();
+            var date = DateTime.UtcNow.AddMonths(-1);
+            for (int i = 0; i < 10; i++)
+            {
+                var period = $"{date:yyyy-MM}";
+                periodComboBox.Items.Add(period);
+                date = date.AddMonths(-1);
+            }
+            periodComboBox.SelectedIndex = 0;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
